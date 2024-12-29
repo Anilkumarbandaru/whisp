df8ecf47f95e8e2bdb6bc9914a70351b5a5fb490
Anilkumarbandaru <Anilkumarbandaru98@gmail.com>
2024-12-30 00:03:40 +0530

    Server side Auth Integration done

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -6,26 +6,74 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 import React from 'react'
 import { useState } from "react"
+import { toast } from "sonner"
+import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants.js"
+import { apiClient } from "@/lib/api-client"
+import { useNavigate } from "react-router-dom"
+
 
 const Auth = () => {
 
+  const navigate = useNavigate();
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
+
+  const validateLogin = () => {
+    if(!email.length) {
+      toast.error("Email is required");
+      return false;
+    }
+    if(!password.length) {
+      toast.error("Password is required");
+      return false;
+    }
+    return true;
+  }
+
+  const validateSignup = () => {
+    if(!email.length) {
+      toast.error("Email is required");
+      return false;
+    }
+    if(!password.length) {
+      toast.error("Password is required");
+      return false;
+    }
+    if(password !== confirmPassword) {
+      toast.error("Password and Confirm Password should be same");
+      return false; 
+    }
+    return true;
+  }
+
   const handleLogin = async () => {
-
-  }
+    if(validateLogin()) {
+      const response = await apiClient.post(LOGIN_ROUTE ,{email,password}, {withCredentials:true})
+      if(response.data.user.id) {
+        if(response.data.user.profileSetup) navigate("/chat");
+        else navigate("/profile");
+        }
+      }
+    }
+  
   const handleSignup = async () => {
+    if(validateSignup()) {
+      const response = await apiClient.post(SIGNUP_ROUTE,{email,password}, {withCredentials:true})
+      if(response.status===201) {
+        navigate("/profile");
+      }
+    }
+  };
 
-  }
 
 
   return (
     <div className='h-[100vh] w-[100vw] flex items-center justify-center'>
       <div className='h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] 
-    xl:w-[60vw] rounded-3xl grid xl:grid-cols-2'>
-        <div className='flex flex-col items-center justify-center h-full gap-5'>
-          <div className='text-center'>
+    xl:w-[60vw] rounded-3xl flex flex-col xl:flex-row'>
+        <div className='flex flex-col items-center justify-center h-full gap-5 w-full'>
+          <div className='text-center items-center justify-center'>
             <div className='flex items-center justify-center'>
               <h1 className='text-5xl font-bold md:text-6xl'>Welcome</h1>
               <img src={Victory} alt="Victory Emoji" className="h-[100px]" />
@@ -35,7 +83,7 @@ const Auth = () => {
             </p>
           </div>
           <div className="flex items-center justify-center w-full">
-            <Tabs className="w-3/4">
+            <Tabs className="w-3/4" defaultValue="login">
               <TabsList className="bg-transparent rounded-none w-full">
                 <TabsTrigger value="login" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Login</TabsTrigger>
                 <TabsTrigger value="signup" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Signup</TabsTrigger>
@@ -58,6 +106,5 @@ const Auth = () => {
     </div>
 
   )
-}
-
+} 
 export default Auth
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,33 +1,73 @@
-import User from "../models/UserModel";
-import { sign } from "jsonwebtoken";
+import { compare } from "bcrypt";
+import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
 
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 const createToken = ( email, userId) => {
-    return sign({ email, userId}, process.env.JWT_KEY,{expiresIn: maxAge})
+    return jwt.sign({ email, userId}, process.env.JWT_KEY,{expiresIn: maxAge})
 }
 
-export const signup = async (req, res, next) => {
+export const signup = async (request, response, next) => {
     try {
-        const { email, password } = req.body;
+        const { email, password } = request.body;
         if(!email || !password){
-            return res.status(400).send("Email abd Password is required")
+            return request.status(400).send("Email abd Password is required")
         }
         const user = await User.create({email,password});
-        res.cookie("jwt",createToken(email,user.id),{
+        response.cookie("jwt",createToken(email,user.id),{
             maxAge,
             secure: true,
             sameSite: "None"
         });
-        return res.status(201).json({
+        return response.status(201).json({
             user: {
                 id: user.id,
                 email: user.email,
                 profileSetup: user.profileSetup,
             }
         });
+        
     }
     catch (error) {
         console.log({ error });
-        return res.status(500).send("Internal Server Error")
+        return response.status(500).send("Internal Server Error")
+    }
+}
+
+export const login = async (request, response, next) => {
+    try {
+        const { email, password } = request.body;
+        if(!email || !password){
+            return response.status(400).send("Email abd Password is required")
+        }
+        const user = await User.findOne({email});
+        if(!user) {
+            return response.status(404).send("User not found")
+        }
+
+        const auth = await compare(password, user.password);
+        if(!auth) {
+            return response.status(400).send("Password in Incorrect")
+        }
+        response.cookie("jwt",createToken(email,user.id),{
+            maxAge,
+            secure: true,
+            sameSite: "None"
+        });
+        return response.status(200).json({
+            user: {
+                id: user.id,
+                email: user.email,
+                profileSetup: user.profileSetup,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                image: user.image,
+                color : user.color
+            }
+        });
+    }
+    catch (error) {
+        console.log({ error });
+        return response.status(500).send("Internal Server Error")
     }
 }
