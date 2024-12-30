d106cd29ab8a7d33159b73c53eca43b4931ad56c
Anilkumarbandaru <Anilkumarbandaru98@gmail.com>
2024-12-30 12:08:04 +0530

    Added Zustand for JWT Token verification and middleware

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -10,11 +10,13 @@ import { toast } from "sonner"
 import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants.js"
 import { apiClient } from "@/lib/api-client"
 import { useNavigate } from "react-router-dom"
+import { useAppStore } from "@/store"
 
 
 const Auth = () => {
 
   const navigate = useNavigate();
+  const { setUserInfo } = useAppStore();
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("")
@@ -51,6 +53,7 @@ const Auth = () => {
     if(validateLogin()) {
       const response = await apiClient.post(LOGIN_ROUTE ,{email,password}, {withCredentials:true})
       if(response.data.user.id) {
+        setUserInfo(response.data.user)
         if(response.data.user.profileSetup) navigate("/chat");
         else navigate("/profile");
         }
@@ -61,6 +64,7 @@ const Auth = () => {
     if(validateSignup()) {
       const response = await apiClient.post(SIGNUP_ROUTE,{email,password}, {withCredentials:true})
       if(response.status===201) {
+        setUserInfo(response.data.user)
         navigate("/profile");
       }
     }
